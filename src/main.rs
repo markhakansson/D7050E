c068f51e726dc08e304277a35b24cc3626f80f8e
Mark Hakansson <mark@hakansson.live.se>
2019-09-12 14:40:13 +0200

    Refactor of the parse function.

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,19 @@ use std::error::Error;
 use std::fmt;
 use std::result::Result;
 
-use nom::{  IResult,
-            branch::{alt,permutation},
-            FindSubstring,
-            named,
-            character::{complete::{ digit1,
-                                    alpha1,
-                                    multispace0,
-                                    anychar,
-                                    alphanumeric1
-                                    },
-                        },
-            sequence::{delimited,preceded,pair,tuple,terminated},
-            bytes::complete::{tag,is_not,take_till1,take_while1,take_while},
-            combinator::{map,map_parser,peek},
-            multi::{many0_count, fold_many0}
-        };
+use nom::{
+    branch::{alt, permutation},
+    bytes::complete::{is_not, tag, take_till1, take_while, take_while1},
+    character::complete::{alpha1, alphanumeric1, anychar, digit1, multispace0},
+    combinator::{map, map_parser, peek},
+    multi::{fold_many0, many0_count},
+    named,
+    sequence::{delimited, pair, preceded, terminated, tuple},
+    FindSubstring, IResult,
+};
 
 // Identifier for variables etc.
-#[derive(PartialEq,Debug,Eq)]
+#[derive(PartialEq, Debug, Eq)]
 struct Identifier(String);
 
 impl Identifier {
@@ -34,7 +28,7 @@ impl Identifier {
     }
 }
 
-#[derive(Debug,PartialEq,Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum Operator {
     Minus,
     Plus,
@@ -43,7 +37,7 @@ enum Operator {
     Modulo,
 }
 
-#[derive(Debug,PartialEq,Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum Type {
     Int32,
     Bool,
@@ -57,10 +51,10 @@ enum Expr {
     Let(Identifier, Type, Box<Expr>),
 }
 
-fn parse_identifier(input: &str) -> IResult<&str,Identifier> {
+fn parse_identifier(input: &str) -> IResult<&str, Identifier> {
     preceded(
         multispace0,
-        map(alphanumeric1, |s: &str| Identifier::new(s))
+        map(alphanumeric1, |s: &str| Identifier::new(s)),
     )(input)
 }
 
@@ -68,50 +62,31 @@ fn parse_identifier(input: &str) -> IResult<&str,Identifier> {
 fn parse_type(input: &str) -> IResult<&str, Type> {
     alt((
         map(tag("i32"), |_| Type::Int32),
-        map(tag("bool"), |_| Type::Bool)
+        map(tag("bool"), |_| Type::Bool),
     ))(input)
 }
 
 // Parses declaration of a variable
-fn parse_declaration(input: &str) -> IResult<&str,Expr> {
-    let (_,(id, type_lit, expr)): (&str,(Identifier,Type,Expr)) = tuple((
+fn parse_declaration(input: &str) -> IResult<&str, Expr> {
+    let (_, (id, type_lit, expr)): (&str, (Identifier, Type, Expr)) = tuple((
         preceded(
             multispace0,
-            preceded(
-                tag("let"),
-                preceded(
-                    multispace0,
-                    parse_identifier
-                )
-            )
+            preceded(tag("let"), preceded(multispace0, parse_identifier)),
         ),
         preceded(
             multispace0,
-            preceded(
-                tag(":"),
-                preceded(
-                    multispace0, 
-                    parse_type
-                )
-            )
+            preceded(tag(":"), preceded(multispace0, parse_type)),
         ),
         preceded(
             multispace0,
-            preceded(
-                tag("="),
-                preceded(
-                    multispace0,
-                    parse_expr
-                )
-            )
-        )
+            preceded(tag("="), preceded(multispace0, parse_expr)),
+        ),
     ))(input)?;
 
-    Ok(("",Expr::Let(id,type_lit,Box::new(expr))))
-
+    Ok(("", Expr::Let(id, type_lit, Box::new(expr))))
 }
 
-fn parse_op(input: &str) -> IResult<&str,Operator> {
+fn parse_binop(input: &str) -> IResult<&str, Operator> {
     preceded(
         multispace0,
         terminated(
@@ -120,70 +95,63 @@ fn parse_op(input: &str) -> IResult<&str,Operator> {
                 map(tag("%"), |_| Operator::Modulo),
                 map(tag("*"), |_| Operator::Multiply),
                 map(tag("-"), |_| Operator::Minus),
-                map(tag("+"), |_| Operator::Plus)
+                map(tag("+"), |_| Operator::Plus),
             )),
-            multispace0
-        )
-    )(input)    
+            multispace0,
+        ),
+    )(input)
 }
 
-fn parse_i32(input: &str) -> IResult<&str,i32> {
-    let (substring,sign) = fold_many0(
-        preceded(
-            multispace0,
-            terminated(
-                tag("-"),
-                multispace0
-            )
-        ),
+fn parse_i32(input: &str) -> IResult<&str, i32> {
+    let (substring, sign) = fold_many0(
+        preceded(multispace0, terminated(tag("-"), multispace0)),
         1,
         |mut sign: i32, _| {
-            sign = sign*-1;
-                sign
-        }
+            sign = sign * -1;
+            sign
+        },
     )(input)?;
 
-    let (substring,digit) = preceded(
-        multispace0,
-        terminated(
-            digit1,
-            multispace0
-        )
-    )(substring)?;
+    let (substring, digit) = preceded(multispace0, terminated(digit1, multispace0))(substring)?;
 
-    Ok((substring,digit.parse::<i32>().unwrap() * sign))
+    Ok((substring, digit.parse::<i32>().unwrap() * sign))
+}
 
+// Parses binomial/arithmetic expressions
+fn parse_bin_expr(input: &str) -> IResult<&str, Expr> {
+    let (substring, digit) = parse_i32(input)?;
+    if substring == ";" {
+        return Ok(("", Expr::Num(digit)));
+    } else {
+        let (substring, operator) = parse_binop(substring).unwrap();
+
+        return Ok((
+            "",
+            Expr::BinOp(
+                Box::new(Expr::Num(digit)),
+                operator,
+                Box::new(parse_expr(substring).unwrap().1),
+            ),
+        ));
+    }
 }
 
 // NEEDS a reimplementation
 // Should fix priority between operators
 fn parse_expr(input: &str) -> IResult<&str, Expr> {
-
-    // Find i32
-    if let Ok((substring, digit)) = parse_i32(input) {
-        if substring == ";" {
-            return Ok(("",Expr::Num(digit)))
-        } else {
-    
-            let (substring,operator) = parse_op(substring).unwrap();
-
-            return Ok ((
-                "", 
-                Expr::BinOp(
-                Box::new(Expr::Num(digit)),
-                operator,
-                Box::new(parse_expr(substring).unwrap().1)
-                )
-            ))
-        }
-    }
-
-    panic!()
-
-} 
+    alt((
+        parse_bin_expr,
+        parse_declaration
+    ))(input)
+}
 
 fn main() {
     let sum = "   10  + 2 +  3  +  4  + --------     3000         ;";
-    let tree = parse_expr(sum);
-    println!("{:#?}",tree); 
+    let decl = "let a: i32 = 3 + 2 - 5;";
+    let tree_sum = parse_expr(sum);
+    let tree_decl = parse_expr(decl);
+    println!("{:#?}", tree_sum);
+    println!("{:#?}", tree_decl);
+    
+    
 }
